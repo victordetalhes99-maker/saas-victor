17dd2b3940ca813699c5f42698a86434d4e7d65d
gestorsilva74-dot <victordetalhes99@gmail.com>
2026-07-27 01:11:27 -0300

    ATT-7

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "nitro";
 export default defineConfig({
   preset: "cloudflare_module",
   renderer: false,
-  compatibilityDate: "2024-09-19",
+  compatibilityDate: "2025-09-15",
   cloudflare: {
     deployConfig: true,
     nodeCompat: true,
